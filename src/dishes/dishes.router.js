4959bfdfb60c637a5b39016e7eece227524ac48c
chloechao <chloechao@indeed.com>
2024-08-05 08:21:56 +0900

    fix chained method calls

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -5,7 +5,7 @@ const notFound = require("../errors/notFound");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
 
-router.route("/:dishId").get(controller.read).put(controller.update).delete(methodNotAllowed).all(notFound);
-router.route("/").get(controller.list).post(controller.create).delete(methodNotAllowed).all(notFound);
+router.route("/:dishId").get(controller.read).put(controller.update).all(methodNotAllowed);
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
 module.exports = router;
